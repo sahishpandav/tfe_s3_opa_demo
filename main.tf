953dad4c51731885b608dbb63f55555fdbdd76f8
Sahish <sahishpandav@gmail.com>
2025-03-07 17:50:21 +0530

    adding public access commit

--- main.tf
+++ main.tf
@@ -21,3 +21,12 @@ resource "aws_s3_bucket" "example" {
     Environment = "Dev"
   }
 }
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.example.my-tf-test-bucket-7thMarch
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
